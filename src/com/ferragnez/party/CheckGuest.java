c14ff62e4e9d5f2d6a42369b935d37c4f6ced4ba
marco-gulino <marcogulino98@gmail.com>
2025-04-30 17:55:52 +0200

    [feat] add guest name variable obtained through Scanner and trimming functionality in CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,11 +1,26 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
         // Vars
         String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
                 "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+        // Name Check
+        System.out.println("Inserire nome e cognome...");
+        String user = input.nextLine();
+
+        // Remove whitespace from both ends of user
+        // and extra whitespace between name and surname
+        String trimUser = user.trim().replaceAll("\\s+", " ");
+
+        // DEBUG
+        System.out.println(trimUser);
 
     }
 }
