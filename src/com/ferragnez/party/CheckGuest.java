a84219eb83f39ab886a8991683fbf712df4f43a1
marco-gulino <marcogulino98@gmail.com>
2025-04-30 17:25:04 +0200

    [feat] add String array of guests to CheckGuest class

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -3,5 +3,9 @@ package com.ferragnez.party;
 public class CheckGuest {
     public static void main(String[] args) {
 
+        // Vars
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
     }
 }
